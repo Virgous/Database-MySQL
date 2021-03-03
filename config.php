5ff50034b3661f3ab09ec968069eceb7eb2eaee4
Virgous <virgousnur@gmail.com>
2021-03-03 22:17:42 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -2,16 +2,13 @@
 /**
  * using mysqli_connect for database connection
  */
-/*$databaseHost = 'remotemysql.com';
-$databaseName = 'GuFz4QOsys';
-$databaseUsername = 'GuFz4QOsys';
-$databasePassword = 'LchV84T4d5';*/
+
 $databaseHost = 'localhost';
 $databaseName = 'arkademy';
 $databaseUsername = 'root';
 $databasePassword = '';
  
-//$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-$mysqli = new mysqli($databaseHost,$databaseUsername,$databasePassword,$databaseName);
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+//$mysqli = new mysqli($databaseHost,$databaseUsername,$databasePassword,$databaseName);
  
 ?>
